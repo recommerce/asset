d0b638b5e601d9216868a146f15b4493a57b4962
Dumoulin Martin <martin.dumoulin@recommerce.com>
2015-11-16 18:59:08 +0100

    - Add some unit test to increase coverage

--- tests/Asset/AssetClientTest.php
+++ tests/Asset/AssetClientTest.php
@@ -66,6 +66,23 @@ class AssetClientTest extends \PHPUnit_Framework_TestCase
         $this->assertSame($localFile, $instance->get($assetFile, $localFile));
     }
 
+    /**
+     * @expectedException \Recommerce\Asset\Exception\AssetGetException
+     */
+    public function testGetWithTransferError()
+    {
+        $assetFile = 'anyfile.txt';
+        $localFile = '/tmp/newfilename.txt';
+
+        $instance = $this->buildInstance(['internalGet']);
+        $instance
+            ->expects($this->once())
+            ->method('internalGet')
+            ->willReturn(false);
+
+        $this->assertSame($localFile, $instance->get($assetFile, $localFile));
+    }
+
     public function testMove()
     {
         $oldAssetFile = 'anyfile.txt';
@@ -159,6 +176,168 @@ class AssetClientTest extends \PHPUnit_Framework_TestCase
             $newAssetFile,
             $instance->move($oldAssetFile, $destAssetDir, AssetClientInterface::OVERWRITE)
         );
+    }
+
+    public function testListFiles()
+    {
+        $dir = 'someDir';
+
+        $files = [
+            'anyFile1.txt',
+            'anyFile2.xml'
+        ];
+
+        $instance = $this->buildInstance(['internalGetFiles']);
+        $instance
+            ->method('internalGetFiles')
+            ->with($this->equalTo($dir))
+            ->willReturn($files);
+
+        $this->assertSame(
+            $files,
+            $instance->listFiles('someDir')
+        );
+    }
+
+    public function testListFilesWithPattern()
+    {
+        $dir = 'someDir';
+        $pattern = 'File1';
+
+        $files = [
+            'someDir/anyFile1.txt',
+            'someDir/anyFile2.xml'
+        ];
+
+        $instance = $this->buildInstance(['internalGetFiles']);
+        $instance
+            ->method('internalGetFiles')
+            ->with($this->equalTo($dir))
+            ->willReturn($files);
+
+        $this->assertSame(
+            ['someDir/anyFile1.txt'],
+            $instance->listFiles('someDir', $pattern)
+        );
+    }
+
+    public function testRemoveExistingFile()
+    {
+        $assetFile = 'anyFile1.txt';
+
+        $instance = $this->buildInstance(['exists', 'internalRemove']);
+        $instance
+            ->expects($this->once())
+            ->method('exists')
+            ->with($this->equalTo($assetFile))
+            ->willReturn(false);
+
+        $this->assertTrue($instance->remove($assetFile));
+    }
+
+    public function testRemoveNonExistingFile()
+    {
+        $assetFile = 'anyFile1.txt';
+        $internalRemoveReturn = true;
+
+        $instance = $this->buildInstance(['exists', 'internalRemove']);
+        $instance
+            ->expects($this->once())
+            ->method('exists')
+            ->with($this->equalTo($assetFile))
+            ->willReturn(true);
+        $instance
+            ->expects($this->once())
+            ->method('internalRemove')
+            ->with($this->equalTo($assetFile))
+            ->willReturn($internalRemoveReturn);
+
+        $this->assertSame($internalRemoveReturn, $instance->remove($assetFile));
+    }
+
+    public function testRemoveFiles()
+    {
+        $assetFiles = [
+            'assetFile1.txt',
+            'assetFile2.xml'
+        ];
+
+        $instance = $this->buildInstance(['remove']);
+        $instance
+            ->expects($this->exactly(count($assetFiles)))
+            ->method('remove')
+            ->withConsecutive(
+                ['assetFile1.txt'],
+                ['assetFile2.xml']
+            );
+
+        $this->assertTrue($instance->removeFiles($assetFiles));
+    }
+
+    public function testExists()
+    {
+        $assetFile = 'mydir/anyFile1.txt';
+
+        $assetFiles = [
+            'mydir/anyFile1.txt',
+            'mydir/anyFile2.xml'
+        ];
+
+        $instance = $this->buildInstance(['getFiles']);
+        $instance
+            ->expects($this->once())
+            ->method('getFiles')
+            ->with($this->equalTo('mydir'))
+            ->willReturn($assetFiles);
+
+        $this->assertTrue($instance->exists($assetFile));
+    }
+
+    public function testDoesNotExist()
+    {
+        $assetFile = 'mydir/anyFile3.txt';
+
+        $assetFiles = [
+            'mydir/anyFile1.txt',
+            'mydir/anyFile2.xml'
+        ];
+
+        $instance = $this->buildInstance(['getFiles']);
+        $instance
+            ->expects($this->once())
+            ->method('getFiles')
+            ->with($this->equalTo('mydir'))
+            ->willReturn($assetFiles);
+
+        $this->assertFalse($instance->exists($assetFile));
+    }
+
+    /**
+     * @dataProvider getFilesAssetDirs
+     */
+    public function testGetFiles($assetDir, $expectedParameter)
+    {
+        $fileList = [
+            'anyFile1.txt',
+            'anyFile2.xml'
+        ];
+
+        $instance = $this->buildInstance(['internalGetFiles']);
+        $instance
+            ->expects($this->once())
+            ->method('internalGetFiles')
+            ->with($this->equalTo($expectedParameter))
+            ->willReturn($fileList);
+
+        $this->assertSame($fileList, $instance->getFiles($assetDir));
+    }
+
+    public function getFilesAssetDirs()
+    {
+        return [
+            ['.', ''],
+            ['someDir', 'someDir']
+        ];
     }
 
     private function buildInstance(array $methods = [])
