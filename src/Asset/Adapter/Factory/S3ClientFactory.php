55c934ae270762e4e48617222bf04fb5d8f16928
Nicolas Cayet <nicolas.cayet@recommerce.com>
2021-04-23 15:15:44 +0200

    support params token for S3Client factory

--- src/Asset/Adapter/Factory/S3ClientFactory.php
+++ src/Asset/Adapter/Factory/S3ClientFactory.php
@@ -12,10 +12,15 @@ class S3ClientFactory implements AssetFactoryInterface
      */
     public function create(array $params)
     {
-        $s3client = \Aws\S3\S3Client::factory([
+        $s3clientParams = [
             'key' => $params['key'],
             'secret' => $params['secret']
-        ]);
+        ];
+        if (isset($params['token'])) {
+            $s3clientParams['token'] = $params['token'];
+        }
+
+        $s3client = \Aws\S3\S3Client::factory($s3clientParams);
         $bucket = $params['bucket'];
 
         return new S3Client($s3client, $bucket, $params);
